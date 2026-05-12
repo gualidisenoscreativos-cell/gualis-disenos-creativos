f6a1de57827ab1c5720774cf50367a7cbaae2637
User <user@example.com>
2026-05-12 19:21:20 -0300

    Initial commit

---
+++ app/blackHanSans.tsx
@@ -0,0 +1,6 @@
+import { Black_Han_Sans } from "next/font/google";
+
+const blackHanSans = Black_Han_Sans({
+  subsets: ["latin"],
+  weight: "400",
+});
---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-pink-100 flex items-center justify-center">
+
+      <div className="bg-white p-10 rounded-3xl shadow-2xl text-center">
+
+        <h1 className="text-6xl font-extrabold text-pink-500 mb-5">
+          Gualis 🌈
+        </h1>
+
+        <h2 className="text-3xl font-bold text-violet-500 mb-5">
+          Diseños Creativos
+        </h2>
+
+        <p className="text-xl text-gray-600 max-w-xl">
+          Plataforma kawaii para stickers IA,
+          imprimibles y herramientas para emprender ✨
+        </p>
+
+      </div>
+
+    </main>
+  )
+}
